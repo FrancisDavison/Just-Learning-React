8d262c45dd4b53188bc3189d79d9b7d7dcad948f
FrancisDavison <francis.nj.davison@gmail.com>
2021-02-01 19:15:14 +0000

    Setting up MySQL integration

--- Just_Learning_React/src/main/java/com/example/Just_Learning_React/JustLearningReactApplication.java
+++ Just_Learning_React/src/main/java/com/example/Just_Learning_React/JustLearningReactApplication.java
@@ -2,8 +2,10 @@ package com.example.Just_Learning_React;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JustLearningReactApplication {
 
 	public static void main(String[] args) {
