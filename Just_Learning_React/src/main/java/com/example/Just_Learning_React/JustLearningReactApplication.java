9b22ea1b8e0127a649a87e5742e4182932de012c
FrancisDavison <francis.nj.davison@gmail.com>
2021-02-01 19:08:29 +0000

    Setting up files for project

---
+++ Just_Learning_React/src/main/java/com/example/Just_Learning_React/JustLearningReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.Just_Learning_React;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustLearningReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustLearningReactApplication.class, args);
+	}
+
+}
---
+++ Just_Learning_React/src/test/java/com/example/Just_Learning_React/JustLearningReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Just_Learning_React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustLearningReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
